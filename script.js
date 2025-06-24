2e3a1b7e5e8698326484057eeeb0c1e80184784e
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-24 14:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //your JS code here. If required.
+let table = document.getElementById("table");
+
+let body = document.createElement("tbody");
+body.id = "output";
+
+body.innerHTML = `<tr>
+<td colspan=2>Loading...</td>
+</tr>
+`;
+
+table.appendChild(body);
+
+function randomPromise(index) {
+  const delay = Math.floor(Math.random() * 2000 + 1000);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({index,time:(delay/1000).toFixed(2)});
+    }, delay);
+  });
+}
+
+const promise1 = randomPromise(1);
+const promise2 = randomPromise(2);
+const promise3 = randomPromise(3);
+
+Promise.all([promise1, promise2, promise3]).then((result) => {
+  body.innerHTML = ""; // Clear the "Loading..." row
+  let timeTaken = 0;
+
+  result.forEach(({ index, time }) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>Promise ${index}</td>
+      <td>${time} </td>
+    `;
+    timeTaken+=time
+    body.appendChild(row);
+  });
+
+  const maxTime = Math.max(...result.map(r=> parseFloat(r.time).toFixed(3)));
+
+  const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>Total</td>
+      <td>${maxTime}</td>
+    `;
+
+    body.appendChild(row);
+});
+
